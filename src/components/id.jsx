da4dedb3455b5755730a754cc62e3fa206f00ca3
yassin samir <yassinsam3@gmail.com>
2022-10-21 18:38:04 +0200

    fixing bugs with fetching data

--- src/components/id.jsx
+++ src/components/id.jsx
@@ -1,26 +1,20 @@
 import { createContext } from "react";
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import classes from "../css/id.module.css";
 export const context = createContext(classes.link);
 function Id() {
   const data = useLoaderData();
-  const navigate = useNavigate();
   console.log(data);
   if (data.type == "p") return data;
-  const { title, body, id } = data;
+  const { name, email, id } = data;
   return (
     <div className={classes.id}>
       <p className={classes.idp}>{id}</p>
-      <p>{title}</p>
-      <p>{body}</p>
-      <p
-        onClick={() => {
-          navigate("/");
-        }}
-        className={classes.link}
-      >
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+      <Link to="/" className={classes.link}>
         Back to main page
-      </p>
+      </Link>
     </div>
   );
 }
